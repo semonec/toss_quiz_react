d1b057befee8434298efdfcc638dc6d86c25cc3c
Park <sempark@ea.com>
2019-08-07 16:14:25 +0900

    상대경로로 다시

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export class App extends Component<{}, AppState> {
         'X-Naver-Client-Secret': 'G130WKaANl',
       },
     };
-    axios.get(`https://openapi.naver.com/v1/search/news.json?query=${this.state.text}`, config)
+    axios.get(`/v1/search/news.json?query=${this.state.text}`, config)
       .then(response => {
         console.log(response);
         let items: [] = response.data.items;
